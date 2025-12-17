9bb4caf8b2c5705ab9bc71ac6759f2968520d818
Tristan Hascoet <tristan@prediction002.r-ccs27.riken.jp>
2025-12-17 10:26:29 +0900

    Update pipelines init with new classes

--- diffhydro/pipelines/__init__.py
+++ diffhydro/pipelines/__init__.py
@@ -1,3 +1,3 @@
 from .routing_calibration import CalibrationRouter, CalibrationModule
-from .routing_learning import LearnedRouter
-from .base_e2e import  BaseE2EPipeline
+from .routing_learning import LearnedRouter, LearningModule
+from .routing_runoff_learning import  BaseE2EPipeline

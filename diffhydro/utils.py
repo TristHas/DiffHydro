e23397a28f6fedd4b99c06eb96f3dda0aed8286d
Tristan Hascoet <tristan@prediction002.r-ccs27.riken.jp>
2025-12-17 10:25:08 +0900

    Allow custom var_y in nse_fn

--- diffhydro/utils.py
+++ diffhydro/utils.py
@@ -8,7 +8,8 @@ import torch
 import torch.nn as nn
 
 def nse_fn(o: torch.Tensor, lbl: torch.Tensor, 
-               eps: float = 1e-12, dim: int = -1):
+           var_y = None,
+           eps: float = 1e-12, dim: int = -1):
     """
         Memory efficient NSE function using the formula:
         MSE(o,lbl)=⟨o^2⟩ + ⟨lbl^2⟩ − 2⟨o·lbl⟩
@@ -18,7 +19,8 @@ def nse_fn(o: torch.Tensor, lbl: torch.Tensor,
     ss_ll = torch.einsum('...t,...t->...', lbl, lbl)
     ss_ol = torch.einsum('...t,...t->...', o,   lbl)
     mse   = (ss_oo + ss_ll - 2*ss_ol) / T
-    var_y = torch.var(lbl, dim=dim, unbiased=False)
+    if var_y is None:
+        var_y = torch.var(lbl, dim=dim, unbiased=False)
     return 1.0 - mse / (var_y.clamp_min(eps))
 
 
@@ -26,7 +28,8 @@ class SimpleTimeSeriesSampler(nn.Module):
     def __init__(self, x, y, init_len, pred_len):
         super().__init__()
         assert (x.index==y.index).all(), "Index misalignment"
-        assert (x.columns==y.columns).all(), "Columns misalignment"
+        if len(x.columns)==len(y.columns):
+            assert (x.columns==y.columns).all(), "Columns misalignment"
         self.x = x
         self.y = y
         self.init_len = init_len

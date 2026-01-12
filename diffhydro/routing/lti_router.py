d62f4a7f2ef5e092c9aefe96198937c8c724e512
Tristan Hascoet <tristan@prediction002.r-ccs27.riken.jp>
2026-01-12 10:08:50 +0900

    Make staged vs. non-staged routing transparent to diffhydro router

--- diffhydro/routing/lti_router.py
+++ diffhydro/routing/lti_router.py
@@ -1,14 +1,13 @@
-# routers_wrappers.py
-import torch.nn as nn
 import itertools
+import torch.nn as nn
+import xtensor as xt
 
-from ..structs import DataTensor
 from ..structs.time_series import (
     ensure_bst_dims,
-    datatensor_like,
-    to_coord_sequence,
 )
+
 from diffroute import (
+    RivTree,
     LTIRouter as LTIRouterCore,
     LTIStagedRouter as LTIStagedRouterCore,
 )
@@ -17,81 +16,64 @@ class LTIRouter(nn.Module):
     """
         Wrapper class around diffroute.LTIRouter.
     """
-    def __init__(self, **kwargs):
+    def __init__(self, *args, **kwargs):
         super().__init__()
         self.core = LTIRouterCore(**kwargs)
+        self.staged_core = LTIStagedRouterCore(**kwargs)
 
-    def forward(self, runoff_df: DataTensor, g, params=None) -> DataTensor:
-        ensure_bst_dims(runoff_df)
-        y = self.core(runoff_df.values, g, params)
-        return datatensor_like(runoff_df, y)
-
-class LTIStagedRouter(nn.Module):
-    """
-        Wrapper class around diffroute.LTIStagedRouter
-    """
-    def __init__(self, **kwargs):
-        super().__init__()
-        self.core = LTIStagedRouterCore(**kwargs)
-
-    def forward(self, runoff_df: DataTensor, g, params=None) -> DataTensor:
+    def forward(self, runoff: xt.DataTensor, g, params=None) -> xt.DataTensor:
         """
         """
-        return self.route_all_clusters(runoff_df, g, params)
+        router = self.core if isinstance(g, RivTree) else self.staged_core
+        discharge = router(runoff.values, g, params) 
+        return xt.DataTensor(discharge, dims=runoff.dims, coords=runoff.coords)
+
+    ###
+    ### Iterative routing procedure helpers
+    ###
+    def route_clusters_sequence(self, runoff_iter, gs, params=None):
+        """
+        """
+        meta_q = []
         
+        def xs_tensor_gen(runoff_iter):
+            for x in runoff_iter:
+                meta_q.append((x.dims, x.coords))
+                yield x.values
+        
+        core_iter = self.staged_core.route_all_clusters_yield(xs_tensor_gen(runoff_iter), 
+                                                               gs, params=params)
+        
+        for discharge in core_iter:
+            dims, coords = meta_q.pop(0)
+            yield xt.DataTensor(discharge, dims=dims, coords=coords)
+    
     def route_one_cluster(self,
-                          x_df: DataTensor,
+                          runoff: xt.DataTensor,
                           gs,
                           cluster_idx: int,
                           params=None,
                           transfer_bucket=None):
         """
         """
-        y_c, transfer_bucket = self.core.route_one_cluster(
-            x_df.values, gs, cluster_idx, params, transfer_bucket
+        y_c, transfer_bucket = self.staged_core.route_one_cluster(
+            runoff.values, gs, cluster_idx, 
+            params, transfer_bucket
         )
-        return datatensor_like(x_df, y_c)
+        return xt.DataTensor(y_c, dims=runoff.dims, coords=runoff.coords)
     
-    def route_all_clusters(self, x_df: DataTensor, gs,
-                           params=None, 
-                           display_progress=False) -> DataTensor:
+    def init_upstream_discharges(self, runoff, gs, cluster_idx, 
+                                 params=None):
         """
         """
-        y = self.core.route_all_clusters(x_df.values, gs, 
-                                         params=params, 
-                                         display_progress=display_progress)
-        spatial_coords = to_coord_sequence(gs.nodes_idx)
-        return datatensor_like(x_df, y, spatial_coords=spatial_coords)
-
-    def route_all_clusters_yield(self, xs_df, gs, 
-                                 params=None, 
-                                 display_progress=False):
-        """
-        """
-        xs_for_labels, xs_for_tensors = itertools.tee(xs_df)
-        xs_tensor_gen = (x_df.values for x_df in xs_for_tensors)
-        core_iter = self.core.route_all_clusters_yield(
-            xs_tensor_gen, gs, params=params, 
-            display_progress=display_progress
-        )
-        
-        for x_df, y_tensor in zip(xs_for_labels, core_iter):
-            yield datatensor_like(x_df, y_tensor)
-
-    def init_upstream_discharges(self, xs_df, gs, cluster_idx, 
-                                 params=None, 
-                                 display_progress=False):
-        """
-        """
-        if isinstance(xs_df, DataTensor):
-            xs_tensor_gen = (xs_df.values[:, s:e] for s, e in gs.node_ranges)
+        if isinstance(runoff, xt.DataTensor):
+            xs_tensor_gen = (runoff.values[:, s:e] for s, e in gs.node_ranges)
         else:
-            xs_tensor_gen = (x_df.values for x_df in xs_df) 
-        return self.core.init_upstream_discharges(
+            xs_tensor_gen = (x_df.values for x_df in runoff) 
+        return self.staged_core.init_upstream_discharges(
              xs_tensor_gen, gs,
              cluster_idx,
              params=params,
-             display_progress=display_progress
         )
 
-    
+LTIStagedRouter = LTIRouter
